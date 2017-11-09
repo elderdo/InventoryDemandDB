647290ca0a3f654fed2ce4650b206e05912910d2
Douglas Elder <douglas.s.elder@boeing.com>
2017-11-09 16:25:05 -0600

    prefixed filenames with a date timestamp for table amd_warner_robins_files

---
+++ Database/Sql/changeFilenameForAmdWarnerRobinsFiles.sql
@@ -0,0 +1,30 @@
+/* Formatted on 11/7/2017 12:08:05 AM (QP5 v5.287) */
+UPDATE amd_warner_robins_files
+   SET filename = '2017_06_12_T-F77_Demand.xlsx'
+ WHERE filename = 'T-F77 Demand_061217.xlsx';
+
+COMMIT;
+
+UPDATE amd_warner_robins_files
+   SET filename = '2017_07_03_T-F77_Demand.xlsx'
+ WHERE filename = 'T-F77 Demand_070317.xlsx';
+
+COMMIT;
+
+UPDATE amd_warner_robins_files
+   SET filename = '2017_08_07_T-F77_Demand.xlsx'
+ WHERE filename = 'T-F77 Demand_080717.xlsx';
+
+COMMIT;
+
+UPDATE amd_warner_robins_files
+   SET filename = '2017_10_02_T-F77_Demand.xlsx'
+ WHERE filename = 'T-F77 Demand_2017_10_02.xlsx';
+
+COMMIT;
+
+UPDATE amd_warner_robins_files
+   SET filename = '2017_09_25_T-F77_Demand.xlsx'
+ WHERE filename = 'T-F77 Demand_2017_09_25.xlsx';
+
+COMMIT;
